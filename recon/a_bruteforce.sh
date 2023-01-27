26807fc38b69baef4e7e3b00075dde58c0f73b97
Alejandro Sol <alexander.soltesz1@gmail.com>
2023-01-27 17:50:51 +0100

    some commentary on bruteforce sh, nothing major. not integrated anywhere for now

--- recon/a_bruteforce.sh
+++ recon/a_bruteforce.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-
-run_install() {
-  apt-get install "${REQUIRED_APT_LIBRARIES[@]}"
-}
-check_install() {
-  # apt-get install golang-go
-
-  # go install github.com/OJ/gobuster/v3@latest
-
-  REQUIRED_APT_LIBRARIES=("golang-go")
-  REQUIRED_GO_LIBRARIES=("github.com/OJ/gobuster/v3@latest")
-  dpkg -s "${REQUIRED_LIBRARIES[@]}" >/dev/null 2>&1 || run_install
-}
-
+###############################################
+#  Not currently added to the recon process   #
+###############################################
 go_bust(){
     # check common stuff
     admin:admin
@@ -23,8 +12,6 @@ go_bust(){
 
     #gobuster small
     gobuster dir --url http://{target_IP} --wordlist {wordlist_lication}/directory-list-2.3-small.txt
-
-
 }
 
 mysql_access(){
@@ -35,15 +22,9 @@ mysql -h {target_ip} -u root
 ftp_access(){
 ftp {target_ip}
 # login with anonymous - 230 login successful check msg.
-
 }
 
 main() {
-  # run_install
-  # get_wordlists
-  # check_install
-  # check_resources
-
   # mysql_access
   # ftp_access
 }
